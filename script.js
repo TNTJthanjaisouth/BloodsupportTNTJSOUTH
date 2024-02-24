d13e5ad54b37a8129f3fbc1fff21a5954581b298
Mohammadibbu <mohammadibbu008@gmail.com>
2024-02-24 23:20:36 +0530

    new

--- script.js
+++ script.js
@@ -128,3 +128,21 @@ fetchData().then((td) => {
     setTimeout(searchRecords, 1000);
   });
 });
+
+// Menu-toggle button
+
+$(document).ready(function () {
+  $(".menu-icon").on("click", function () {
+    $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("black");
+  } else {
+    $("nav").removeClass("black");
+  }
+});
