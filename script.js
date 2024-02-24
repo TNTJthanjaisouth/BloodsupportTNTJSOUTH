538f732f83e4acc4c97953dfcae3843e65c69149
Mohammadibbu <mohammadibbu008@gmail.com>
2024-02-24 15:31:25 +0530

    new

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { fetchData, orderedData } from "./FetchDataSheet.js";
+
 const Branch_ = document.getElementById("Branch");
 const Bloodtype = document.getElementById("Blood_Type");
 const Search = document.getElementById("search");
@@ -6,19 +7,15 @@ const Avail_Or_Not = document.getElementById("Avail_Or_Not");
 const tbody = document.getElementById("tbody");
 const thead = document.getElementById("thead");
 
-let array = [{ name: "john", Branch: "thiruvallur" }];
-
-//--------------------function-------------------
+// Function to add filtered data to HTML
 function AddFilterinHtml(filteredData) {
   thead.innerHTML = "";
   tbody.innerHTML = "";
 
-  const { ...headings } = filteredData[0];
+  const headings = Object.keys(filteredData[0]);
   thead.insertAdjacentHTML(
     "beforeend",
-    Object.keys(headings)
-      .map((col) => `<th class="">${col}</th>`)
-      .join("")
+    headings.map((col) => `<th class="">${col}</th>`).join("")
   );
 
   filteredData.forEach((row, index) => {
@@ -33,52 +30,54 @@ function AddFilterinHtml(filteredData) {
   });
 }
 
-//------------------------------------------------------
+// Fetch data and populate dropdowns
 fetchData().then((td) => {
   console.log(td);
   const tableData = td;
-  // console.log(tableData);
   const Bloodtype_opt = [];
   const Branch = [];
 
   tableData.forEach((x, y) => {
     Bloodtype_opt.push(x.BloodGroup);
     Branch.push(x.Branch);
-    // console.log(x);
   });
 
   const Blood_Type_opt = [...new Set(Bloodtype_opt)];
   const Branch_opt = [...new Set(Branch)];
+
   Branch_opt.forEach((val) => {
-    const option = document.createElement("option");
-    option.innerHTML = val;
-    option.value = val;
-
-    option.className = "option";
-    Branch_.appendChild(option);
+    addOptionToDropdown(Branch_, val);
   });
+
   Blood_Type_opt.forEach((val) => {
-    const option = document.createElement("option");
-    option.innerHTML = val;
-    option.className = "option";
-    option.value = val;
-    Bloodtype.appendChild(option);
+    addOptionToDropdown(Bloodtype, val);
   });
-  //-------------------------------------------------------
-  function error(id, errMess) {
-    document.getElementById(`${id}`).innerHTML = "";
-    document.getElementById(`${id}`).removeAttribute("class");
 
-    setTimeout((e) => {
-      document.getElementById(`${id}`).innerHTML = errMess;
-      document.getElementById(`${id}`).className = "Error";
-    }, 1000);
+  function addOptionToDropdown(dropdown, value) {
+    const option = document.createElement("option");
+    option.innerHTML = value;
+    option.value = value;
+    option.className = "option";
+    dropdown.appendChild(option);
   }
-  // ----------------------------------------------
-  function SearchData(id1, id2, id3) {
-    const value1 = id1.value;
-    const value2 = id2.value;
-    const value3 = id3.value;
+
+  // Function to handle error message display
+  function displayError(id, errMess) {
+    const elem = document.getElementById(id);
+    elem.innerHTML = "";
+    elem.removeAttribute("class");
+
+    setTimeout(() => {
+      elem.innerHTML = errMess;
+      elem.className = "Error";
+    }, 200);
+  }
+
+  // Function to handle search functionality
+  function searchRecords() {
+    const value1 = Bloodtype.value;
+    const value2 = Branch_.value;
+    const value3 = Avail_Or_Not.value;
 
     if (value1 === "" || value2 === "" || value3 === "") {
       alert("Please select all given fields");
@@ -88,20 +87,12 @@ fetchData().then((td) => {
     let filter;
     if (value2 === "all" && value3 === "all") {
       filter = orderedData(tableData, value1, "BloodGroup");
-    } else if (value2 === "all") {
-      filter = orderedData(tableData, value1, "BloodGroup");
-      filter = orderedData(filter, value2, "Branch");
-      filter = orderedData(filter, value3, "AvailableOrNot");
-    } else if (value3 == "all") {
-      filter = orderedData(tableData, value1, "BloodGroup");
-      filter = orderedData(filter, value2, "Branch");
-      filter = orderedData(filter, value3, "AvailableOrNot");
     } else {
       filter = orderedData(tableData, value1, "BloodGroup");
       filter = orderedData(filter, value2, "Branch");
       filter = orderedData(filter, value3, "AvailableOrNot");
     }
-    // console.table(filter);
+
     if (filter.length > 0) {
       AddFilterinHtml(filter);
       document.getElementById("err").innerHTML = "";
@@ -109,24 +100,9 @@ fetchData().then((td) => {
     } else {
       thead.innerHTML = "";
       tbody.innerHTML = "";
-      error("err", "No Record Found !");
+      displayError("err", "No Record Found !");
     }
   }
-  Search.addEventListener("click", () => {
-    console.log(Bloodtype.value);
-    console.log(Branch_.value);
-    console.log(Avail_Or_Not.value);
 
-    SearchData(Bloodtype, Branch_, Avail_Or_Not);
-  });
+  Search.addEventListener("click", searchRecords);
 });
-// console.log(Blood_Type_opt, Branch_opt);
-
-function Listener(variable, eL) {
-  variable.addEventListener(`${eL}`, () => {
-    console.log(variable.value);
-  });
-}
-// Listener(Branch_, "change");
-// Listener(Bloodtype, "change");
-// Listener(Avail_Or_Not, "change");
